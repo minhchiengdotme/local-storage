d7f6c197c60cb99450afb09b1311efca4765e50b
minhchiengdotme <contact@minhchieng.me>
2023-08-23 17:21:42 +0700

    coding

---
+++ database/migrations/2023_08_23_093130_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable(false)->default('');
+            $table->string('slug', 255)->nullable(false)->default('');
+            $table->string('code_begin', 3)->nullable(false)->default('000');
+            $table->string('description', 255)->nullable(false)->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
